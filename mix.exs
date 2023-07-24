663bea3494d0036db9a4bd643ac8ed51a1c3d39e
Samara Motta <samaramotta@1998@gmail.com>
2023-07-24 18:45:37 +0300

    upgrade phoenix to 1.7.7

--- mix.exs
+++ mix.exs
@@ -43,16 +43,16 @@ defmodule Jelly.MixProject do
   # Type `mix help deps` for examples and options.
   defp deps do
     [
-      {:phoenix, "~> 1.7.0-rc.3", override: true},
+      {:phoenix, "~> 1.7.7"},
       {:phoenix_ecto, "~> 4.4"},
       {:ecto_sql, "~> 3.6"},
       {:postgrex, ">= 0.0.0"},
       {:phoenix_html, "~> 3.3"},
       {:phoenix_live_reload, "~> 1.2", only: :dev},
-      {:phoenix_live_view, "~> 0.18.14"},
-      {:heroicons, "~> 0.5"},
+      {:phoenix_live_view, "~> 0.19.5"},
+      {:heroicons, "~> 0.5.3"},
       {:floki, ">= 0.30.0", only: :test},
-      {:phoenix_live_dashboard, "~> 0.7.2"},
+      {:phoenix_live_dashboard, "~> 0.8.0"},
       {:esbuild, "~> 0.5", runtime: Mix.env() == :dev},
       {:tailwind, "~> 0.1.8", runtime: Mix.env() == :dev},
       {:swoosh, "~> 1.3"},
